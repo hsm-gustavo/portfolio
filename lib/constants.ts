910a0cf37ab8c4a7d46ee271b915864933fa74b6
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2025-03-26 22:39:43 -0300

    feat: theme provider, utils for shadcn, zod validations for contact forms, types and constants for the .env

---
+++ lib/constants.ts
@@ -0,0 +1,2 @@
+export const GITHUB_KEY = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
---
+++ lib/types.ts
@@ -0,0 +1,33 @@
+export interface GitHubData {
+  repositories: {
+    totalCount: number
+  }
+  starredRepositories: {
+    totalCount: number
+  }
+  contributionsCollection: {
+    contributionCalendar: {
+      totalContributions: number
+      weeks: Array<{
+        contributionDays: Array<{
+          contributionCount: number
+          date: string
+        }>
+      }>
+    }
+  }
+}
+
+export interface ContributionCalendar {
+  totalContributions: number
+  weeks: Array<{
+    contributionDays: Array<{
+      contributionCount: number
+      date: string
+    }>
+  }>
+}
+
+export interface GitHubCalendarProps {
+  contributionData?: ContributionCalendar
+}

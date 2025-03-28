d185f0a1d4d4f7a0608f7c4ffceb8fdd8cb84173
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2025-03-28 02:25:53 -0300

    feat: new translations and routing

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,4 +3,10 @@ import { defineRouting } from "next-intl/routing"
 export const routing = defineRouting({
   locales: ["en", "pt-BR"],
   defaultLocale: "en",
+  localePrefix: {
+    mode: "always",
+    prefixes: {
+      "pt-BR": "/br"
+    }
+  }
 })

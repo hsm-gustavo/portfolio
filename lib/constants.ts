e3a50589e35d441aca22faa4dc9cc38ac82ef734
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2025-07-19 09:10:01 -0300

    fix: update email transport configuration to use secure port 465

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,4 +3,4 @@ export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || ""
 export const MAIL_HOST = process.env.MAIL_HOST
 export const MAIL_USER = process.env.MAIL_USER
 export const MAIL_PASS = process.env.MAIL_PASS
-export const MAIL_PORT = 587
+export const MAIL_PORT = 465
